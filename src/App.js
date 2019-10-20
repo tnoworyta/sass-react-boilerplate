ca0263261af2fe8feaa79f039848eccd388d1114
Tomasz Noworyta <tomasz.noworyta@gmail.com>
2019-10-20 15:51:57 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="home">
+      <header className="navbar__header">
+        <strong>Zabdos</strong>
+        <nav className="navbar__menu">
+          <a href="f" className="navbar__menu-item">
+            Flight Statuses
+          </a>
+          <a href="c" className="navbar__menu-item btn">
+            Check in
+          </a>
+          <a href="c" className="navbar__menu-item">
+            <img src="/av.jpg" alt="avatar" />
+          </a>
+        </nav>
       </header>
+      <div className="intro">
+        <div className="intro__tagline">
+          <div className="intro__tagline--highlight">Fly with us to</div>
+          <div className="intro__tagline--normal"></div>
+          <div className="intro__tagline--cal">
+            <div className="intro__tagline--day-split">
+              <div className="intro__tagline--day-color1"></div>
+              <div className="intro__tagline--day-color2"></div>
+            </div>
+            <div className="intro__tagline--day"></div>
+            <div className="intro__tagline--day"></div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
